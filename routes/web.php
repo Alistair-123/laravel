59299cd59cdfe201f078b751fa1482bf8b467260
Alistair-123 <alixtairloberternos@gmail.com>
2024-09-11 17:23:00 +0800

    alistair humana

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+})->name('home');
+
+Route::get('/item', function () {
+    return view('item');
+})->name('item');
+
+Route::get('/picture', function () {
+    return view('picture');
+})->name('picture');
